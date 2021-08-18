54e288b01ae0cf1ce329c3cf1629b83a340ddad4
Aaron Barlow <abarlow@hydrotexlube.com>
2021-08-18 10:07:05 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {  // (e) is the event object provided
+    e.preventDefault() //default behavior of forms is to completely refresh the page. This made sense in the old days,
+    // but now we have access to good client side javascript. preventDefault() prevents this default behavior.
+
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location ).then( (response) => {
+        response.json().then( (data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
+
---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const express = require('express')
+const hbs = require('hbs')
+
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')   //gets absolute path to directory where index.html is
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath))    //provides path to express.static that configures application  
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Great Scott'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Great Scott'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is the help message!',
+        title: 'Help',
+        name: 'Aaron'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if(error) {
+            return res.send({ error }) // object { error: error } moved to shorthand { error }
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error }) // object { error: error } moved to shorthand { error }
+            }
+            res.send({
+                forecast: forecastData,
+                location, // 'location: location,' moved to shorthand 'location,'
+                address: req.query.address
+            })
+            
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Aaron'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Aaron'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
